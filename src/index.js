3fa6192be3ca79802a834bf532194d92ee0afbb5
Veladzic Senahid <losloboss@yahoo.com>
2019-09-26 17:38:08 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "jquery"
+import "popper.js"
+import "bootstrap"
+import "./main.scss"
+import "./toggler"
+import "./scroll"
+
+
+/* CAROUSEL */
+
+$('.carousel').carousel( {
+    pause: false,
+})
+
+
+
---
+++ src/toggler.js
@@ -0,0 +1,24 @@
+
+/* MAIN NAV LOGIC */
+const toggler = document.querySelector(".navbar-toggler")
+const barOne = document.querySelector(".bar1")
+const barTwo = document.querySelector(".bar2")
+const barTre = document.querySelector(".bar3")
+
+
+
+toggler.addEventListener("click", e => {
+    barOne.classList.toggle("change1")
+    barTwo.classList.toggle("change2")
+    barTre.classList.toggle("change3")
+})
+/* MAIN NAV LOGIC END*/
+
+
+
+
+  
+
+
+
+
