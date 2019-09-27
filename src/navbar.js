cf14e900528e6abe443b33d8540cb60bef216be3
Veladzic Senahid <losloboss@yahoo.com>
2019-09-27 15:04:11 +0200

    modal changes

--- src/navbar.js
+++ src/navbar.js
@@ -6,12 +6,11 @@ const contact = document.querySelector(".nav-link--contact")
 
 window.onscroll = function() {
     if(window.scrollY >= 50) {
-        nav.classList.add("scaleMe")
+        nav.classList.add("scaleMe")    /* ScaleMe and ShowMe classes are in the nav.scss */
         contact.classList.add("showMe")
        
     }  else {
         contact.classList.remove("showMe")
-
     }
 } 
 
